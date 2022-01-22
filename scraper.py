864a98ac21bf6c7d15e41ef6bbe59808e949c4be
kylechui <kyle.chui@pm.me>
2022-01-21 22:52:28 -0800

    Clean up scraping code.

--- scraper.py
+++ scraper.py
@@ -3,23 +3,45 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-fileID = open("./Courses/IDs.txt", "w")
-fileDescription = open("./Courses/descriptions.txt", "w")
-
 driver = webdriver.Chrome("/usr/bin/chromedriver")
+# TODO: Pass the department as a parameter from the C++ main file, use "update" command
 driver.get("https://registrar.ucla.edu/academics/course-descriptions?search=Mathematics")
+# Defining some macros
+lowerDivPrefix = "//div[starts-with(@id, 'lower-division-courses')]/div[@class='course-record']"
+upperDivPrefix = "//div[starts-with(@id, 'upper-division-courses')]/div[@class='course-record']"
+
+
 # Wait for page elements to load before grabbing them
 try:
     elem = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.XPATH, "//div[@class='course-record']/h3"))
+        # EC.presence_of_element_located((By.XPATH, "//div[@class='course-record']/h3"))
+        EC.presence_of_element_located((By.XPATH, lowerDivPrefix))
     )
 finally:
-    courseTitles = driver.find_elements(By.XPATH, "//div[@class='course-record']/h3")
-    courseDescriptions = driver.find_elements(By.XPATH, "//div[@class='course-record']/p[2]")
+    lowerDivHeaders = driver.find_elements(By.XPATH, lowerDivPrefix + "/h3")
+    lowerDivDescriptions = driver.find_elements(By.XPATH, lowerDivPrefix + "/p[2]")
+    upperDivHeaders = driver.find_elements(By.XPATH, upperDivPrefix + "/h3")
+    upperDivDescriptions = driver.find_elements(By.XPATH, upperDivPrefix + "/p[2]")
 
-    fileID.writelines([title.get_attribute('innerText')[:title.get_attribute('innerText').find('.')] + '\n' for title in courseTitles])
-    fileDescription.writelines([course.get_attribute('innerText') + '\n' for course in courseDescriptions])
+    # Add lower/upper div course IDs to a text file
+    fileID = open("./Courses/IDs.txt", "w")
+    for header in lowerDivHeaders:
+        title = header.get_attribute('innerText')
+        fileID.write(title[:title.find('.')] + '\n')
+    fileID.close()
+    fileID = open("./Courses/IDs.txt", "a")
+    for header in upperDivHeaders:
+        title = header.get_attribute('innerText')
+        fileID.write(title[:title.find('.')] + '\n')
+    fileID.close()
+
+    # Add lower/upper div course descriptions to a text file
+    fileDescription = open("./Courses/descriptions.txt", "w")
+    for description in lowerDivDescriptions:
+        fileDescription.write(description.get_attribute('innerText') + '\n')
+    fileDescription.close();
+    fileDescription = open("./Courses/descriptions.txt", "a")
+    for description in upperDivDescriptions:
+        fileDescription.write(description.get_attribute('innerText') + '\n')
+    fileDescription.close();
     driver.quit()
-
-fileID.close()
-fileDescription.close()
